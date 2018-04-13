4febd8f2e7176ea01d2f5fce48049144b4662aa2
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-04-13 12:33:19 +0200

    UserDao class completed with all methods

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -2,12 +2,17 @@ package pl.coderslab;
 
 import java.sql.Connection;
 
+import pl.coderslab.dao.UserDao;
+import pl.coderslab.entity.User;
+
 public class Main {
 	public static void main(String[] args) {
-		try (Connection c = DbUtil.getConnection();) {
-			
-		} catch (Exception e) {
-			System.out.println("Error: "+e.getMessage());
-		}
+		
+		
+		UserDao.showUser(UserDao.readAllByGroupId(1)[0]);
+		User user1 = UserDao.readByEmail("janiNAnowaK@onet.pl");
+		user1.setEmail("JaninaNowak@gmail.com");
+		UserDao.update(user1);
+		UserDao.showUser(UserDao.readByEmail("janinanowak@Gmail.com"));
 	}
 }
--- src/pl/coderslab/dao/UserDao.java
+++ src/pl/coderslab/dao/UserDao.java
@@ -3,30 +3,42 @@ package pl.coderslab.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import pl.coderslab.DbUtil;
 import pl.coderslab.entity.User;
 
 public class UserDao {
-		private static final String CREATE_USER_QUERY = "INSERT INTO users(name,email,password,user_group_id) VALUES (?,?,?,?)";
+		private static final String CREATE_USER_QUERY = "INSERT INTO users (name, email, password, user_group_id) VALUES (?, ?, ?, ?)";
 		private static final String READ_USER_QUERY = "Select * from users where id = ?"; 
 		private static final String UPDATE_USER_QUERY = "UPDATE users SET name = ? , email = ?, password = ?, user_group_id =? WHERE id = ?";
 		private static final String DELETE_USER_QUERY = "DELETE FROM users where id = ?"; 
 		private static final String READ_ALL_USERS_QUERY = "SELECT * FROM users";
+		private static final String READ_ALL_USERS_BY_GROUP_ID_QUERY = "Select * from users where user_group_id = ?";
+		private static final String READ_USER_BY_EMAIL_QUERY = "Select * from users where email=?";
 		
 		
-		public void delete(int userId) {
-			try (Connection connection = DbUtil.getConnection(); 
-					PreparedStatement statement = connection.prepareStatement(DELETE_USER_QUERY);) { 
-						statement.setInt(1, userId);
-						statement.executeUpdate(); 
-			} catch (Exception e) {
-				System.out.println("Error: "); 
-				e.printStackTrace();
-				}
+		public static void showUser (User user) {
+			System.out.println("Id: " + user.getId() + ", name: " + user.getName() + ", email: " + user.getEmail() + ", password: " + user.getPassword() + ", user_group_id: " + user.getUserGroupId());
+		}
+		
+		public static void create(User user) {
+			try (Connection connection = DbUtil.getConnection();
+				PreparedStatement statement = connection.prepareStatement(CREATE_USER_QUERY);) {
+			        statement.setString(1, user.getName());
+			        statement.setString(2, user.getEmail());
+			        statement.setString(3, user.getPassword());
+			        statement.setInt(4, user.getUserGroupId());
+			        statement.executeUpdate();
+			} catch (Exception e) { 
+				System.out.println("Error: ");
+				e.printStackTrace(); 
 			}
+		}
 		
-		public User read(int userId) {
+		
+		public static User readById(int userId) {
 			User user = new User();
 			try (Connection connection = DbUtil.getConnection();
 			PreparedStatement statement = connection.prepareStatement(READ_USER_QUERY);) {
@@ -47,20 +59,114 @@ public class UserDao {
 			    return user;
 			}
 		
-		
-		public void update(User user) {
+		public static void update(User user) {
 			try (Connection connection = DbUtil.getConnection();
 					PreparedStatement statement = connection.prepareStatement(UPDATE_USER_QUERY);) {
-				statement.setInt(6, user.getId()); 
+				statement.setInt(5, user.getId()); 
 				statement.setString(1, user.getName()); 
-				statement.setString(3, user.getEmail()); 
-				statement.setString(4, user.getPassword());
-				statement.setInt(5, user.getUserGroupId()); 
+				statement.setString(2, user.getEmail()); 
+				statement.setString(3, user.getPassword());
+				statement.setInt(4, user.getUserGroupId()); 
 				statement.executeUpdate();
 			} catch (Exception e) {
 				System.out.println("Error: ");
 				e.printStackTrace();
 				} 
 			}
+		
+		
+		public static void delete(int userId) {
+			try (Connection connection = DbUtil.getConnection(); 
+					PreparedStatement statement = connection.prepareStatement(DELETE_USER_QUERY);) { 
+						statement.setInt(1, userId);
+						statement.executeUpdate(); 
+			} catch (Exception e) {
+				System.out.println("Error: "); 
+				e.printStackTrace();
+				}
+			}
+		
+		public static User[] readAll() {
+			ArrayList<User> users = new ArrayList<User>();
+			try (Connection connection = DbUtil.getConnection();
+			PreparedStatement statement = connection.prepareStatement(READ_ALL_USERS_QUERY);) {
+				
+
+				ResultSet resultSet = statement.executeQuery(); 
+				while (resultSet.next()) {
+					User loadedUser = new User();
+					loadedUser.setId(resultSet.getInt("id")); 
+					loadedUser.setName(resultSet.getString("name")); 
+					loadedUser.setPassword(resultSet.getString("password")); 
+					loadedUser.setEmail(resultSet.getString("email")); 
+					loadedUser.setUserGroupId(resultSet.getInt("user_group_id"));
+					users.add(loadedUser);
+					}
+		
+			} catch (Exception e) { 
+				System.out.println("Error: ");
+				e.printStackTrace(); 
+				}
+			// tworzą tablicę o takiej samej wielkosci co lista
+			User[] uArray = new User[users.size()]; 
+			// przekopiowują listę do tablicy
+			uArray = users.toArray(uArray); 
+			return uArray;
+			}
+		
+		public static User[] readAllByGroupId(int groupId) {
+			ArrayList<User> users = new ArrayList<User>();
+			try (Connection connection = DbUtil.getConnection();
+			PreparedStatement statement = connection.prepareStatement(READ_ALL_USERS_BY_GROUP_ID_QUERY);) {
+				
+				statement.setInt(1, groupId);
+				ResultSet resultSet = statement.executeQuery(); 
+				while (resultSet.next()) {
+					User loadedUser = new User();
+					loadedUser.setId(resultSet.getInt("id")); 
+					loadedUser.setName(resultSet.getString("name")); 
+					loadedUser.setPassword(resultSet.getString("password")); 
+					loadedUser.setEmail(resultSet.getString("email")); 
+					loadedUser.setUserGroupId(resultSet.getInt("user_group_id"));
+					users.add(loadedUser);
+					}
+				
+		
+			} catch (Exception e) { 
+				System.out.println("Error: ");
+				e.printStackTrace(); 
+				}
+			 User[] uArray = new User[users.size()]; 
+			 uArray = users.toArray(uArray);
+			    return uArray;
+		}
+		
+		public static User readByEmail(String userEmail) {
+			String userEmailToLowerCase = userEmail.toLowerCase();
+			User user = new User();
+			try (Connection connection = DbUtil.getConnection();
+			PreparedStatement statement = connection.prepareStatement(READ_USER_BY_EMAIL_QUERY);) {
+			statement.setString(1, userEmailToLowerCase);
+			try (ResultSet resultSet = statement.executeQuery()) {
+				while (resultSet.next()) {
+					user.setId(resultSet.getInt("id")); 
+					user.setName(resultSet.getString("name")); 
+					user.setEmail(resultSet.getString("email")); 
+					user.setPassword(resultSet.getString("password")); 
+					user.setUserGroupId(resultSet.getInt("user_group_id")); 
+					}
+				}
+			} catch (Exception e) { 
+				System.out.println("Error: ");
+				e.printStackTrace(); 
+				}
+			    return user;
+			}
+		
+		
+		}
+			
+			
+	
 			 
-}
+
